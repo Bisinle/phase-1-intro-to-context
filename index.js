3856b44328aae79f78c98c4034ce85dacce6099d
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-06-28 23:37:58 +0300

    lab half done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+
+function createEmployeeRecord(array) {
+  let employee = {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+  return employee;
+}
+
+let testEmployee = createEmployeeRecord(["Gray", "Worm", "Security", 1]);
+// console.log(testEmployee);
+function createEmployeeRecords(array) {
+ let blah = array.map(item =>{
+    return createEmployeeRecord(item)
+ })
+return blah
+
+}
+
+
+
+console.log(createEmployeeRecords(array));
